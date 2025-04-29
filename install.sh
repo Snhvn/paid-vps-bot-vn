575127b83e1577347c5f80e2ad54892b6fcd1157
Snhvn <snhvnnguyen@gmail.com>
2025-04-29 20:24:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@
 
 apt install python3-pip && pip install docker discord paramiko
 wget https://github.com/Snhvn/paid-vps-bot-vn/raw/refs/heads/main/main.py
-https://github.com/Snhvn/paid-vps-bot-vn/raw/refs/heads/main/boot.py
+wget https://github.com/Snhvn/paid-vps-bot-vn/raw/refs/heads/main/boot.py
 wget https://github.com/Snhvn/paid-vps-bot-vn/raw/refs/heads/main/node-install.sh && sh node-install.sh
 echo "Vui lòng nhập token bot Discord của bạn, Tạo một bot tại discord.dev và nhận token, Bạn không cần bất kỳ ý định nào:"
 read -r DISCORD_TOKEN
