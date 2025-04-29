d8d00a7fe17f41d3639def2e2ca00ec0bd596f01
Snhvn <snhvnnguyen@gmail.com>
2025-04-29 13:44:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+        echo ".........................................................." 
+        echo ".....By SNIPAVN................................") 
+        echo ".........................................................." 
+        echo "........#####...######...##....#######........####......." 
+        echo "......##........##...##..##..  ##....##......##..##......" 
+        echo "..........##....##...##..##....##.##.##.....###..###......"
+        echo "..........##....##...##.##.....##...........##....##......"
+        echo "......#####.....##...####......##..........###....##......"
+        echo ".........................................................."
+        echo "..Discord - https://dsc.gg/servertipacvn ..") 
+        echo ".........................................................."
+
+apt install python3-pip && pip install udocker discord
 echo "Vui lòng nhập token bot Discord của bạn, Tạo một bot tại discord.dev và nhận token, Bạn không cần bất kỳ ý định nào:"
 read -r DISCORD_TOKEN
 echo "Updating main.py with the provided Discord token.../Đang cập nhật main.py bằng token bot Discord được cung cấp..."
