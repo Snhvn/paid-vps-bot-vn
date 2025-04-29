cf4c91b4738a10d83cef07bbb81f18cc680106f9
Snhvn <snhvnnguyen@gmail.com>
2025-04-29 17:14:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@
 
 apt install python3-pip && pip install docker discord paramiko
 wget https://github.com/Snhvn/paid-vps-bot-vn/raw/refs/heads/main/main.py
+https://github.com/Snhvn/paid-vps-bot-vn/raw/refs/heads/main/boot.py
 wget https://github.com/Snhvn/paid-vps-bot-vn/raw/refs/heads/main/node-install.sh && sh node-install.sh
 echo "Vui lòng nhập token bot Discord của bạn, Tạo một bot tại discord.dev và nhận token, Bạn không cần bất kỳ ý định nào:"
 read -r DISCORD_TOKEN
@@ -20,6 +21,7 @@ echo "nhập webhook lười ghi quá"
 read -r WEBHOOK_URL
 echo "Updating main.py with the provided Discord token.../Đang cập nhật main.py bằng token bot Discord được cung cấp..."
 sed -i "s/TOKEN = ''/TOKEN = '$DISCORD_TOKEN'/" main.py
+sed -i "s/TOKEN = ''/TOKEN = '$DISCORD_TOKEN'/" boot.py
 sed -i "s/WEBHOOK_URL = ''/WEBHOOK_URL = '$WEBHOOK_URL'/" boot.py
 clear
 echo "nhập cái này 'python3 boot.py'
