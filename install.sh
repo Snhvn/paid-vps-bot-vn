92e5c5f849d2799a0008a2cb392bd57615de9a01
Snhvn <snhvnnguyen@gmail.com>
2025-04-29 13:45:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,10 @@
         echo ".........................................................."
 
 apt install python3-pip && pip install udocker discord
+wget https://github.com/Snhvn/uia/raw/refs/heads/main/main.py
 echo "Vui lòng nhập token bot Discord của bạn, Tạo một bot tại discord.dev và nhận token, Bạn không cần bất kỳ ý định nào:"
 read -r DISCORD_TOKEN
 echo "Updating main.py with the provided Discord token.../Đang cập nhật main.py bằng token bot Discord được cung cấp..."
 sed -i "s/TOKEN = ''/TOKEN = '$DISCORD_TOKEN'/" main.py
 clear
+echo "python3 main.py"
