3b4553c3d378a36149b2714841d7b282e405f700
Snhvn <snhvnnguyen@gmail.com>
2025-04-29 13:49:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@
         echo "..Discord - https://dsc.gg/servertipacvn ..") 
         echo ".........................................................."
 
-apt install python3-pip && pip install udocker discord
+apt install python3-pip && pip install docker discord
 wget https://github.com/Snhvn/uia/raw/refs/heads/main/main.py
 echo "Vui lòng nhập token bot Discord của bạn, Tạo một bot tại discord.dev và nhận token, Bạn không cần bất kỳ ý định nào:"
 read -r DISCORD_TOKEN
